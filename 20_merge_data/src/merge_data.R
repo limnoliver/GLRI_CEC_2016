f8a762e50ce8f59e22e1b71b87ce5a83bd1ee594
Laura DeCicco <ldecicco@usgs.gov>
2017-09-26 09:28:20 -0500

    More workflow

--- 20_merge_data/src/merge_data.R
+++ 20_merge_data/src/merge_data.R
@@ -1,6 +1,9 @@
 library(dplyr)
 library(USGSHydroTools)
 library(tidyr)
+library(readxl)
+library(readr)
+library(dataRetrieval)
 
 merged_neonic_flow <- function(neonic, flow, sites){
 
@@ -74,3 +77,77 @@ merged_NWIS <- function(tracking, NWIS, neonic_flow, pCodeInfo){
   
   return(nwis_neonic)
 }
+
+special_cas <- function(){
+  special_cas <- data.frame(casrn = c("135410-20-7",
+                                    "210880-92-5",
+                                    "165252-70-0",
+                                    "138261-41-3",
+                                    "111988-49-9",
+                                    "153719-23-4"),
+                          CAS = c("Acetamiprid",
+                                  "Clothianidin",
+                                  "Dinotefuran",
+                                  "Imidacloprid",
+                                  "Thiacloprid",
+                                  "Thiamethoxam") ,stringsAsFactors = FALSE)
+}
+
+create_chemData <- function(file_path,neonic_NWIS, special_cas){
+
+  chem_data <- neonic_NWIS %>%
+    select(SiteID = site,
+           `Sample Date` = pdate,
+           Value = value,remark_cd,
+           CAS = chemical) %>%
+    filter(!is.na(Value),
+           !is.na(CAS)) %>%
+    left_join(special_cas, by="CAS") %>%
+    mutate(Value = Value*1000) #ng -> ug
+  
+  chem_data$CAS[!is.na(chem_data$casrn)] <- chem_data$casrn[!is.na(chem_data$casrn)]
+  
+  chem_data <- select(chem_data, -casrn)
+
+  write.csv(chem_data, file = file_path, row.names = FALSE)
+  
+  
+}
+
+
+create_tox_chemInfo <- function(file_path, special_cas, pCodeInfo){
+  
+  chem_data <- read_csv(file_path)
+  
+  chem_info <- select(chem_data, CAS) %>%
+    distinct() %>%
+    left_join(select(pCodeInfo, CAS = casrn,
+                     `Chemical Name` = parameter_nm), by="CAS") %>%
+    left_join(special_cas, by=c("CAS"="casrn"))
+  
+  chem_info$`Chemical Name`[!is.na(chem_info$CAS.y)] <- chem_info$CAS.y[!is.na(chem_info$CAS.y)]
+  
+  chem_info <- select(chem_info, -CAS.y) %>%
+    mutate(Class = "Neonic")
+
+  write.csv(chem_info, file = file_path, row.names = FALSE)
+}
+
+create_tox_siteInfo <- function(file_path, sites){
+
+  siteInfo <- sites %>%
+    select(SiteID = USGS.station.number,
+           `Short Name` = shortName)
+  
+  USGS_site <- readNWISsite(siteInfo$SiteID)
+  
+  siteInfo <- siteInfo %>%
+    left_join(select(USGS_site, SiteID = site_no, 
+                     dec_lat = dec_lat_va,
+                     dec_lon = dec_long_va), by="SiteID") %>%
+    mutate(site_grouping = "All")
+  
+  write.csv(siteInfo, file = file_path, row.names = FALSE)
+  
+}
+  
