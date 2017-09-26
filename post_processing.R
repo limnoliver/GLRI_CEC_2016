723b50c388e688fe3be81facda7068cd974c07f3
Laura DeCicco <ldecicco@usgs.gov>
2017-09-26 10:20:59 -0500

    Workflow post-make

---
+++ post_processing.R
@@ -0,0 +1,25 @@
+library(remake)
+library(toxEval)
+
+make()
+
+chem_data <- make("chem_data")
+chem_info <- make("chem_info")
+chem_site <- make("site_info")
+
+ACClong <- get_ACC(chem_info$CAS)
+ACClong <- remove_flags(ACClong)
+
+cleaned_ep <- clean_endPoint_info(endPointInfo)
+filtered_ep <- filter_groups(cleaned_ep, 
+                             groupCol = "intended_target_family_sub")
+
+chemicalSummary <- get_chemical_summary(ACClong,
+                                        filtered_ep,
+                                        chem_data, 
+                                        chem_site, 
+                                        chem_info)
+chemPlot <- plot_tox_boxplots(chemicalSummary, 
+                              category = "Chemical",
+                              plot_ND = FALSE)
+chemPlot
