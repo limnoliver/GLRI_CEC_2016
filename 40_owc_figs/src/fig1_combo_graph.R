f19d0c1783b1fa356cbb09d3ab1cc040dfbccb26
Laura DeCicco <ldecicco@usgs.gov>
2018-05-11 14:35:57 -0500

    endpoint graph

--- 40_owc_figs/src/fig1_combo_graph.R
+++ 40_owc_figs/src/fig1_combo_graph.R
@@ -1,26 +1,33 @@
-graphData_combos <- function(cs_1, cs_2, cs_3, file_to_save){
+library(toxEval)
+
+graphData_combos <- function(graphData_tox, graphData_wq, graphData_conc, file_to_save){
   
-  graphData_tox <- graph_chem_data(cs_1)
+
   graphData_tox$guide_side <- "ToxCast\nMaximum EAR per Site"
-  
-  graphData_wq <- graph_chem_data(cs_2, sum_logic = FALSE)
   graphData_wq$guide_side <- "Traditional\nMaximum Toxicity Quotient per Site"
-  
-  graphData_conc <- graph_chem_data(cs_3, sum_logic = FALSE)
   graphData_conc$guide_side <- "Concentration [ug/L]"
 
-  browser()
-  
   toxPlot_wq <- combo_plot_matches(graphData_tox, graphData_wq, 
                                    thres_1 = NA, thres_2 = NA, 
                                    drop = FALSE, grid = FALSE, 
                                    gd_3 = graphData_conc,
                                    include_counts = FALSE)
   
+  ggplot2::ggsave(toxPlot_wq, filename = file_to_save, width = 11, height = 9)
+
+}
+
+
+graphData_wq_tox <- function(graphData_tox, graphData_wq, file_to_save){
+  
+
+  graphData_tox$guide_side <- "ToxCast\nMaximum EAR per Site"
+  graphData_wq$guide_side <- "Traditional\nMaximum Toxicity Quotient per Site"
+  
   toxPlot_wq_tox <- combo_plot_matches(graphData_tox, graphData_wq, 
-                                   thres_1 = NA, thres_2 = NA, 
-                                   drop = TRUE, grid = FALSE)
-  
-  
+                                     thres_1 = NA, thres_2 = NA, 
+                                     drop = TRUE, grid = FALSE,include_counts = FALSE)
+
+  ggplot2::ggsave(toxPlot_wq_tox, filename = file_to_save, width = 11, height = 9)
   
 }
