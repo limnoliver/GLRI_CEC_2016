edc6bb18d9ce169582c8a79a25c9219c7568c5f1
Oliver <soliver@usgs.gov>
2018-06-05 15:56:29 -0500

    added processing step to clean up date issues in the data. Also added a column ('source') which just states whether the sample came from the general pesticide schedule, the separate neonic analysis, or the glyphosate analysis. This will help with processing and deciding which pcodes to use in the case of double measures later on.

---
+++ 15_clean_data/clean_dat.R
@@ -0,0 +1,24 @@
+clean_pesticides <- function(dat) {
+  # Milwaukee River has tracking date of 2016-04-07 but sampling date
+  # of 2016-04-05. Will change in data so as not to affect any other data that is merged
+  # with tracking
+  
+  dat$sample_dt[dat$SiteID %in% '04087170' & dat$sample_dt %in% as.Date('2016-04-05')] <- as.Date('2016-04-07')
+  dat$source <- 'pesticides_s2437'
+}
+
+clean_neonics <- function(dat) {
+  
+  # Maumee river site has tracking date of 2016-05-10 but sampling date of 2016-05-03. 
+  # Hladik has 2016-05-03 published as sample date, but this didn't seem to trip up pesticides or glyphosate data,
+  # so am just changing neonic date. 
+  dat$sample_dt[dat$SiteID %in% '04193500' & dat$sample_dt %in% as.Date('2016-05-03')] <- as.Date('2016-05-10')
+  
+  dat$source <- 'neonic'
+  return(dat)
+}
+
+clean_glyphosate <- function(dat) {
+  dat$sample_dt[dat$SiteID %in% '04087170' & dat$sample_dt %in% as.Date('2016-04-05')] <- as.Date('2016-04-07')
+  dat$source <- 'glyphosate'
+}
