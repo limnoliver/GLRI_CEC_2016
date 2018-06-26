37ece44a6b496ed34145296384873b05ab33600f
Samantha Oliver <soliver@usgs.gov>
2018-06-26 15:08:07 -0500

    remove maumee date changes in clean_neonics and glyphosate

--- 15_clean_data/src/clean_dat.R
+++ 15_clean_data/src/clean_dat.R
@@ -6,17 +6,11 @@ clean_pesticides <- function(dat) {
 
 clean_neonics <- function(dat) {
   
-  # Maumee river site has tracking date of 2016-05-10 but sampling date of 2016-05-03. 
-  # Hladik has 2016-05-03 published as sample date, but this didn't seem to trip up pesticides or glyphosate data,
-  # so am just changing neonic date. 
-  dat$sample_dt[dat$SiteID %in% '04193500' & dat$sample_dt %in% as.Date('2016-05-03')] <- as.Date('2016-05-10')
-  
   dat$source <- 'neonic'
   return(dat)
 }
 
 clean_glyphosate <- function(dat) {
-  dat$sample_dt[dat$SiteID %in% '04087170' & dat$sample_dt %in% as.Date('2016-04-05')] <- as.Date('2016-04-07')
   dat$source <- 'glyphosate'
   return(dat)
 }
