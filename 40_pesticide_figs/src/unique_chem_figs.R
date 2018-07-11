240984365e56fc3cb0cd5d1df7091e57a46da2b3
Samantha Oliver <soliver@usgs.gov>
2018-07-11 16:10:17 -0500

    made into function so could work into summary_figs remake flow

--- 40_pesticide_figs/src/unique_chem_figs.R
+++ 40_pesticide_figs/src/unique_chem_figs.R
@@ -1,42 +1,69 @@
-# number of unique chemicals through time by site
-reduced_dat <- make('reduced_dat')
-sites <- unique(reduced_dat$SiteID)
-
-n_unique_time <- setNames(data.frame(matrix(ncol = 3, nrow = 0)), c("SiteID", "sample_dt", "unique_on_date"))
-
-for (i in 1:length(sites)) {
-  temp_dat <- filter(reduced_dat, SiteID == sites[i]) %>%
-    filter(!(remark_cd %in% '<')) %>%
-    select(SiteID, sample_dt, pCode) %>%
-    arrange(sample_dt)
+get_n_unique_chems <- function(reduced_dat, sites) {
   
-  dates <- unique(temp_dat$sample_dt)
-
+  n_unique_time <- setNames(data.frame(matrix(ncol = 3, nrow = 0)), c("SiteID", "sample_dt", "unique_on_date"))
+  site_ids <- unique(reduced_dat$SiteID)
+  site_num_names <- select(sites, site_no, shortName, `Dominant.land.use.`)
   
-  for (j in 1:length(dates)) {
+  for (i in 1:length(site_ids)) {
+    temp_dat <- filter(reduced_dat, SiteID == site_ids[i]) %>%
+      filter(!(remark_cd %in% '<')) %>%
+      select(SiteID, sample_dt, pCode) %>%
+      arrange(sample_dt)
     
-    # calculate number of new chems
-    ordered_dat <- filter(temp_dat, sample_dt <= dates[j]) %>%
-      summarize(unique_on_date = length(unique(pCode))) %>%
-      mutate(SiteID = sites[i], sample_dt = dates[j])
+    dates <- unique(temp_dat$sample_dt)
     
-    n_unique_time <- bind_rows(n_unique_time, ordered_dat)
+    
+    for (j in 1:length(dates)) {
       
+      # calculate number of new chems
+      ordered_dat <- filter(temp_dat, sample_dt <= dates[j]) %>%
+        summarize(unique_on_date = length(unique(pCode))) %>%
+        mutate(SiteID = site_ids[i], sample_dt = dates[j])
+      
+      n_unique_time <- bind_rows(n_unique_time, ordered_dat)
+      
+    }
   }
+  
+  n_unique_time <- group_by(n_unique_time, SiteID) %>%
+    mutate(n_added_on_date = c(NA, diff(unique_on_date))) %>%
+    mutate(n_added_on_date = ifelse(is.na(n_added_on_date), unique_on_date, n_added_on_date)) %>%
+    mutate(sample_no = 1:n()) %>%
+    left_join(site_num_names, by = c('SiteID' = 'site_no'))
+  
+  return(n_unique_time)
+  
 }
 
-n_unique_time <- group_by(n_unique_time, SiteID) %>%
-  mutate(n_added_on_date = c(NA, diff(unique_on_date))) %>%
-  mutate(n_added_on_date = ifelse(is.na(n_added_on_date), unique_on_date, n_added_on_date)) %>%
-  mutate(sample_no = 1:n())
 
-ggplot(n_unique_time, aes(x = sample_dt, y = unique_on_date)) +
-  geom_line(aes(group = SiteID, color = SiteID)) +
-  labs(x = '', y = 'Unique chemicals detected') +
-  theme_bw()
+plot_unique_chems <- function(target_name, unique_chems, type){
+  
+  if (type == 'cumulative') {
+    p <- ggplot(n_unique_time, aes(x = sample_dt, y = unique_on_date)) +
+      geom_line(aes(group = shortName, color = `Dominant.land.use.`)) +
+      labs(x = '', y = 'Unique chemicals detected', color = "Dominant land use") +
+      theme_bw()
+  }
+  
+  if (type == 'new') {
+    # n_after_first <- filter(n_unique_time, sample_no >1)
+    # ggplot(n_after_first, aes(sample_dt, y = n_added_on_date)) +
+    #   geom_line(aes(group = shortName, color = shortName)) +
+    #   facet_wrap(~shortName, ncol = 2, scales = 'free_y') +
+    #   theme_bw()
+    
+    p <- ggplot(n_unique_time, aes(sample_dt, y = n_added_on_date)) +
+      geom_line(aes(group = shortName, color = `Dominant.land.use.`)) +
+      facet_wrap(~shortName, ncol = 2, scales = 'free_y') +
+      theme_bw() +
+      labs(y = 'N unique chemicals added', x = '', color = 'Dominant land use')
+  }
+  
+  ggsave(filename = target_name, p, height = ifelse(type == 'new', 12, 6), width = ifelse(type == 'new', 8, 6))
+  
+  
+  
+}
 
-n_after_first <- filter(n_unique_time, sample_no >1)
-ggplot(n_after_first, aes(sample_dt, y = n_added_on_date)) +
-  geom_line(aes(group = SiteID, color = SiteID)) +
-  facet_wrap(~SiteID, ncol = 2, scales = 'free_y') +
-  theme_bw()
+
+
