afeb02c7bf5589075fa5bffa6f1e21b030415603
Oliver <soliver@usgs.gov>
2019-03-19 16:59:53 -0500

    get health-based screening benchmarks and crosswalk between degradate and parent compounds

--- 10_load_data/src/process_raw_bench.R
+++ 10_load_data/src/process_raw_bench.R
@@ -17,3 +17,22 @@ process_benchmarks <- function(in_file, out_file) {
  write.csv(bench_long, file = out_file, row.names = FALSE)
  
 }
+
+process_hsbls <- function(in_pesticides, in_degradates, out_file) {
+   pest <- read.csv(in_pesticides, stringsAsFactors = FALSE, na.strings = c('', 'NA'))
+   deg <- read.csv(in_degradates, stringsAsFactors = FALSE, na.strings = c('', 'NA'))
+   
+   all <- bind_rows(pest, deg)
+   
+   names(all) <- c('Compound', 'CAS', 'pCode', 'ChemClass', 'EPAMaximumContaminantLevels', 'EPAChronicNoncancerHHBP',
+                   'EPACarcinogenicHHBP', 'USGSChronicNoncancerHBSL', 'USGSCancerHBSL', 'remark')
+   
+   all_long <- all %>%
+      tidyr::gather(key = 'endPoint', value = 'value', EPAMaximumContaminantLevels:USGSCancerHBSL) %>%
+      filter(!is.na(value)) %>%
+      filter(grepl('Noncancer', endPoint)) %>%
+      mutate(value = as.numeric(value)) %>%
+      select(Compound, CAS, endPoint, value, remark)
+   
+   write.csv(all_long, file = out_file, row.names = FALSE)
+}
