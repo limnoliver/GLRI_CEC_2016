c0ec75e6bed124a6f62a3811559625fb5fbf815f
Oliver <soliver@usgs.gov>
2019-04-23 16:04:44 -0500

    fix missing CAS numbers as well as compounds that were not classified correctly. Use classes_fixed

---
+++ 10_load_data/src/fix_classes.R
@@ -0,0 +1,9 @@
+fix_classes <- function(raw_classes) {
+  
+  #fipronil amide currently classified as 'other'
+  #when it should be insecticide degradate
+  raw_classes$Class[raw_classes$CAS == '205650-69-7'] <- 'Deg - Insecticide'
+  raw_classes$Class[raw_classes$CAS == '2327-02-8'] <- 'Deg - Herbicide'
+  
+  return(raw_classes)
+}
--- 10_load_data/src/get_chem_info.R
+++ 10_load_data/src/get_chem_info.R
@@ -6,7 +6,21 @@ get_chem_info <- function(infile) {
            pCode = USGS.Parameter.code, 
            CAS = CASRN, 
            parent_pesticide = Pesticide.group,
-           MlWt = Molecular.weight)
+           MlWt = Molecular.weight) %>%
+    mutate(pCode = as.character(pCode))
+  
+  usgs_pcodes_cas <- dataRetrieval::parameterCdFile %>%
+    select(pCode = parameter_cd, CAS_2 = casrn) %>%
+    filter(pCode %in% info_c$pCode)
+  
+  info_c <- left_join(info_c, usgs_pcodes_cas) %>%
+    select(-CAS) %>%
+    rename(CAS = CAS_2)
+  
+  # filter a few compounds out that have repeated CAS numbers
+  #pCode 68566 (Demethyl hexazinone B)
+  
+  # fix missing parent/compounds
   
   return(info_c)
   
