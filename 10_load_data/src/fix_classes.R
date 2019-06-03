b34aaea420877292420d817d55e67f86e992fa51
Oliver <soliver@usgs.gov>
2019-06-03 08:27:22 -0500

    add health-based screening levels

--- 10_load_data/src/fix_classes.R
+++ 10_load_data/src/fix_classes.R
@@ -4,6 +4,7 @@ fix_classes <- function(raw_classes) {
   #when it should be insecticide degradate
   raw_classes$Class[raw_classes$CAS == '205650-69-7'] <- 'Deg - Insecticide'
   raw_classes$Class[raw_classes$CAS == '2327-02-8'] <- 'Deg - Herbicide'
+  raw_classes$Class[raw_classes$CAS == '35045-02-4'] <- 'Deg - Herbicide'
   
   return(raw_classes)
 }
