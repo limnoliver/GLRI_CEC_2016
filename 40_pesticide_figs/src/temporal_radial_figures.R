c00b7f85f8219e071d94f88ff86c836c14d2af50
Samantha Oliver <soliver@usgs.gov>
2020-03-27 13:13:41 -0500

    moved radial plot figure, changed color to single color.

--- 40_pesticide_figs/src/temporal_radial_figures.R
+++ 40_pesticide_figs/src/temporal_radial_figures.R
@@ -51,16 +51,17 @@ graph_dat <- left_join(graph_dat, sites) %>%
 p <- ggplot(data = graph_dat, aes(x = day, y = max_ear)) +
   geom_hline(yintercept = c(0.001, 0.01, 0.1, 1), color = 'gray50') +
   geom_vline(xintercept = c(1,32,61, 92,122,153,183,214,245,275, 306, 336), color = 'gray80') +
-  geom_point(aes(color = site, size = n_hits), alpha = 0.5, shape = 16) +
+  geom_point(aes(size = n_hits), alpha = 0.5, shape = 16, color = 'darkred') +
   facet_grid(rows = vars(dominant_lu), cols = vars(Class)) +
   #geom_jitter() +
   coord_polar(theta = 'x', start = 0) +
   scale_x_continuous(expand = c(0, 0), breaks = c(1,32,61, 92,122,153,183,214,245,275, 306, 336), labels = c('Jan','','Mar', '', 'May', '', 'Jul', '', 'Sep', '', 'Nov', '')) +
   scale_y_log10() +
+  theme_bw() +
   theme(panel.grid  = element_blank(),
         axis.line = element_blank(),
         strip.background = element_blank(),
         strip.text = element_text(face = 'bold')) +
   labs(x = '', y = 'Max EAR for each site-date-class', color = 'Dominant Land Use', size = '# Chems w/hits')
 
-ggsave('40_pesticide_figs/month_class_hits.png', p, height = 6, width = 8)
+ggsave('figures/ms_figures/month_class_hits.png', p, height = 6, width = 8)
